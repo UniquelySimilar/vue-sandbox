2b1a7883d4a65a8a70c216ab5a4890ecc40ca918
UniquelySimilar <consciousexplorer@gmail.com>
2021-03-26 16:44:48 -0600

    vuetest: vuex module: added namespacing, getters, mutations, and component functionality to use them

--- src/components/vuetest/vuex-module/VuexModule.vue
+++ src/components/vuetest/vuex-module/VuexModule.vue
@@ -1,22 +1,71 @@
 <template>
   <div class="vuex-module">
-    <h3>Users</h3>
     <div class="row">
       <div class="col-md-6">
+        <h4>Users</h4>
         <table class="table table-bordered">
           <thead>
             <tr>
               <th>ID</th>
               <th>NAME</th>
+              <th>GENDER</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="user in users" :key="user.id">
               <td>{{ user.id }}</td>
               <td>{{ user.name }}</td>
+              <td>{{ user.gender }}</td>
             </tr>
           </tbody>
         </table>
+        <button type="button" class="btn btn-secondary" @click="addUser">Add Random User</button>
+      </div>
+
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-12">
+            <h4>Male Users</h4>
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>NAME</th>
+                  <th>GENDER</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="user in maleUsers" :key="user.id">
+                  <td>{{ user.id }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.gender }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <h4>Female Users</h4>
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>NAME</th>
+                  <th>GENDER</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="user in femaleUsers" :key="user.id">
+                  <td>{{ user.id }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.gender }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -24,10 +73,28 @@
 
 <script>
   export default {
+    data() {
+      return {
+        gender: 'male'
+      }
+    },
     computed: {
       users() {
         return this.$store.state.userModule.users;
+      },
+      maleUsers() {
+        return this.$store.getters['userModule/getMaleUsers'];
+      },
+      femaleUsers() {
+        return this.$store.getters['userModule/getFemaleUsers'];
       }
     },
+    methods: {
+      addUser() {
+        let nextGender = this.gender === 'male' ? 'female' : 'male';
+        this.gender = nextGender;
+        this.$store.commit('userModule/addUser', this.gender);
+      }
+    }
   }
 </script>
--- src/components/vuetest/vuex-module/store/modules/user-store-module.js
+++ src/components/vuetest/vuex-module/store/modules/user-store-module.js
@@ -1,16 +1,47 @@
 const userModule = {
+  namespaced: true,
   state: () => ({
     users: [
       {
         id: 1,
-        name: 'Tim'
+        name: 'User1',
+        gender: 'male'
       },
       {
         id: 2,
-        name: 'Debbie'
-      }
+        name: 'User2',
+        gender: 'female'
+      },
+      {
+        id: 3,
+        name: 'User3',
+        gender: 'male'
+      },
+      {
+        id: 4,
+        name: 'User4',
+        gender: 'female'
+      },
     ]
   }),
+  getters: {
+    getMaleUsers: state => {
+      return state.users.filter( user => user.gender === 'male')
+    },
+    getFemaleUsers: state => {
+      return state.users.filter( user => user.gender === 'female')
+    },
+  },
+  mutations: {
+    addUser(state, newGender) {
+      let nextId = state.users.length + 1;
+      state.users.push({
+        id: nextId,
+        name: 'User' + nextId,
+        gender: newGender
+      })
+    }
+  }
 }
 
 export default userModule;
